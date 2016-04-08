d68ea0c75dae056cbc3b9f9c1e9e8f0206123f7d
Jean-Jacques MOIROUX <jeanjacquesmoiroux@hotmail.com>
2016-04-08 22:19:44 +0200

    fin de journee

---
+++ Assets/Script/Camera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	private GameObject	main_player_object;
+	private mainPlayer	main_player_script;
+
+	// Use this for initialization
+	void Start () {
+		this.main_player_object = GameObject.Find ("main_player_group");
+		this.main_player_script = this.main_player_object.GetComponent<mainPlayer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (this.main_player_object.transform.position.x, this.main_player_object.transform.position.y, this.main_player_object.transform.position.z);
+	}
+}
