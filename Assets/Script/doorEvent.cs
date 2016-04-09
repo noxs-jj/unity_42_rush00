2448cc9d008022956d2ba63d4def28412a59d729
Jean-Jacques MOIROUX <jeanjacquesmoiroux@hotmail.com>
2016-04-09 21:47:20 +0200

    door anime

---
+++ Assets/Script/doorEvent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doorEvent : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		// Initialiser en fermeture
+		// initialiser collider physique enable
+	}
+
+	private void OnTriggerEnter2D(){
+		// lancer animation ouverture porte
+		// cacher collider (physique, non pas trigger)
+	}
+
+	private void OnTriggerExit2D(){
+		// lancer annimation fermeture porte
+		///reafficher collider Physique
+	}
+}
