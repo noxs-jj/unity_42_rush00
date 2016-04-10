96889fe7ed84aefbdd92c33c6d3b51757f006560
Florian BARILLOT <fbarillo@e1r3p13.42.fr>
2016-04-10 22:48:19 +0200

    v 1.4

--- Assets/Script/menu script/buttonPause.cs
+++ Assets/Script/menu script/buttonPause.cs
@@ -4,8 +4,12 @@ using UnityEngine.UI;
 
 public class buttonPause : MonoBehaviour {
 
+	private AudioClip loseSong;
+	private AudioClip winSong;
 
 	void Start () {
+		loseSong = Resources.Load ("Audio/Sounds/youWin") as AudioClip;
+		winSong = Resources.Load ("Audio/Sounds/youLose") as AudioClip;
 	}
 
 	public void pause(bool paused) {
@@ -44,12 +48,15 @@ public class buttonPause : MonoBehaviour {
 	}
 
 	public void deadPlayer () {
+		Debug.Log("lose");
+		AudioSource.PlayClipAtPoint (loseSong, Vector3.zero);
 		GameObject.Find ("rangScore").GetComponent<Text> ().text = "Loser";
 		GameObject.Find ("score").GetComponent<Canvas> ().enabled = true;
 		GameObject.Find ("loadScene2").GetComponent<Button> ().enabled = false;
 	}
 
 	public void winPlayer () {
+		AudioSource.PlayClipAtPoint (winSong, Vector3.zero);
 		GameObject.Find ("rangScore").GetComponent<Text> ().text = "Winner";
 		GameObject.Find ("score").GetComponent<Canvas> ().enabled = true;
 		GameObject.Find ("loadScene2").GetComponent<Button> ().enabled = true;
