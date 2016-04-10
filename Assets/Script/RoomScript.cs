2b2c9f3b72595fb292d809c963ed6ba60a530e4f
vinz3872 <vinz3872@gmail.com>
2016-04-10 19:33:35 +0200

    add start pathfinding

--- Assets/Script/RoomScript.cs
+++ Assets/Script/RoomScript.cs
@@ -4,24 +4,59 @@ using System.Collections.Generic;
 
 public class RoomScript : MonoBehaviour {
 
-	private List<RoomScript>	RoomListScript= new List<RoomScript>();
-	public List<GameObject>		RoomList = new List<GameObject>();
-	public bool				isPlayerInRoom = false;
+	[HideInInspector]  public int		LocalCost = -1;
 
+	private RoomScript[]		RoomListScript;
+	public List<GameObject>		RoomList = new List<GameObject>();
+	public bool					isPlayerInRoom = false;
+	private IEnumerator			routineCost;
+	private	bool				isRoutineStarted = false;
+	
 	// Use this for initialization
 	void Start () {
-	
+		isPlayerInRoom = false;
+		int i = 0;
+		RoomListScript = new RoomScript[RoomList.Count];
+		foreach (GameObject room in RoomList) {
+			RoomListScript[i] = room.GetComponent<RoomScript>();
+			i++;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (isPlayerInRoom == true && isRoutineStarted == false) {
+			isRoutineStarted = true;
+			routineCost = GetRoomCost ();
+			StartCoroutine (routineCost);
+		} else if (isPlayerInRoom == false && isRoutineStarted == true)
+			StopCoroutine (routineCost);
+	}
 	
+	
+	public void CalcCost(int cost) {
+		if (LocalCost > cost || LocalCost == -1)
+			LocalCost = cost;
+		foreach (RoomScript room in RoomListScript) {
+			if (room.LocalCost == -1 || room.LocalCost > cost + 1)
+				room.CalcCost(cost + 1);
+		}
+	}
+	
+	public void ResetCost() {
+		LocalCost = -1;
+		foreach (RoomScript room in RoomListScript) {
+			if (room.LocalCost != -1)
+				room.ResetCost();
+		}
 	}
 
-	public void GetRoomCost() {
-
+	public IEnumerator GetRoomCost() {
+		ResetCost ();
+		CalcCost(0);
+		Debug.Log ("CalcCostFinished");
+		yield return new WaitForSeconds (1);
 	}
-
 
 	public void SetPlayerInRoom(bool isHere) {
 		isPlayerInRoom = isHere;
---
+++ Assets/Script/zoneDetectPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class zoneDetectPlayer : MonoBehaviour {
+
+	private RoomScript[]	objs;
+
+	void Start () {
+		this.objs = GetComponentsInChildren<RoomScript> ();
+	}
+
+	private void OnTriggerEnter2D (Collider2D entity) {
+		if ("Player" == entity.tag) {
+			foreach (RoomScript room in this.objs) {
+				room.SetPlayerInRoom (true);
+			}
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D entity){
+		if ("Player" == entity.tag) {
+			foreach (RoomScript room in this.objs) {
+				room.SetPlayerInRoom (false);
+			}
+		}
+	}
+}
