d2b7e9a4631813b086efe25665b762a583011b5b
vinz3872 <vinz3872@gmail.com>
2016-04-10 18:36:04 +0200

    add map checkpoint

---
+++ Assets/Script/RoomScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RoomScript : MonoBehaviour {
+
+	private List<RoomScript>	RoomListScript= new List<RoomScript>();
+	public List<GameObject>		RoomList = new List<GameObject>();
+	public bool				isPlayerInRoom = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GetRoomCost() {
+
+	}
+
+
+	public void SetPlayerInRoom(bool isHere) {
+		isPlayerInRoom = isHere;
+	}
+}
