301f9527eab0a91421ecec0e6cba9530e79ab6e1
Jean-Jacques MOIROUX <jeanjacquesmoiroux@hotmail.com>
2016-04-10 15:17:49 +0200

    prefabs door operationnel

--- Assets/Script/doorEvent.cs
+++ Assets/Script/doorEvent.cs
@@ -2,21 +2,35 @@
 using System.Collections;
 
 public class doorEvent : MonoBehaviour {
-
-
-	// Use this for initialization
+	private EdgeCollider2D	trigger_collider;
+	private BoxCollider2D	door_collider_physic;
+	private Animator		animator_object;
+	
 	void Start () {
-		// Initialiser en fermeture
-		// initialiser collider physique enable
+		this.door_collider_physic = gameObject.GetComponent<BoxCollider2D> ();
+		this.animator_object = gameObject.GetComponent<Animator> ();
 	}
 
-	private void OnTriggerEnter2D(){
-		// lancer animation ouverture porte
-		// cacher collider (physique, non pas trigger)
+	private void OnTriggerEnter2D(Collider2D obj){
+		Debug.Log ("OnTriggerEnter2D IN");
+		StartCoroutine (remove_collider_physic_after_time (0.40f));
+		this.animator_object.SetTrigger ("open_launcher");
+		
 	}
 
-	private void OnTriggerExit2D(){
-		// lancer annimation fermeture porte
-		///reafficher collider Physique
+	private IEnumerator remove_collider_physic_after_time(float delay) {
+		yield return new WaitForSeconds (delay);
+		this.door_collider_physic.enabled = false;
+	}
+
+	private void OnTriggerExit2D(Collider2D obj){
+		Debug.Log ("OnTriggerExit2D OUT");
+		StartCoroutine(put_collider_physic_after_time(0.40f));
+		this.animator_object.SetTrigger ("close_launcher");
+	}
+
+	private IEnumerator put_collider_physic_after_time(float delay) {
+		yield return new WaitForSeconds (delay);
+		this.door_collider_physic.enabled = true;
 	}
 }
